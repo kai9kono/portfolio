6003b8c76fad6e9834d3161395ec4cb97184e276
Kai Kono <kai9kono@gmail.com>
2024-02-04 03:47:01 +0900

    Finished Studies2 and Studies2 EN

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,4 @@
-// var enBio = "Born in Tokyo in 2003, raised in California from the age of 7 to 13. Entered Tokyo Gakugei University International Secondary School after returning to Japan. After graduation, to experience a new environment, entered Kwansei Gakuin University and majored in architecture. Hobbies are PC gaming and going on drives."
+// var enBio = "Born in Tokyo in 2003, raised in California from the age of 7 to 13. Entered Tokyo Gakugei University International Secondary School after returning to Japan. After graduation, to experience a new environment, entered Kwansei Gakuin University and majored in architecture. Hobbies are PC gaming and going on drives."
 // var jpBio = "2003年に東京で生まれ、7歳から13歳の間アメリカのカリフォルニア州にて育ち、帰国後に東京学芸大学附属国際中等教育学校に編入。大学は新たな環境を体験するために兵庫の関西学院大学に入学し、建築学を専攻。趣味はPCゲームとドライブ。"
 
 // var enStudies = "At Kwansei Gakuin University, I am studying architecture."
@@ -35,46 +35,6 @@
 // }
 
 
-//1st slideshow
-
-var slideIndex1 = 1;
-showDivs1(slideIndex1);
-
-function moveSlide1(n) {
-  showDivs1(slideIndex1 += n);
-}
-
-function showDivs1(n) {
-  var i;
-  var x = document.getElementsByClassName("studies-img1");
-  if (n > x.length) {slideIndex1 = 1}
-  if (n < 1) {slideIndex1 = x.length}
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
-  }
-  x[slideIndex1-1].style.display = "block";  
-}
-
-//2nd slideshow
-
-var slideIndex2 = 1;
-showDivs2(slideIndex2);
-
-function moveSlide2(n) {
-  showDivs2(slideIndex2 += n);
-}
-
-function showDivs2(n) {
-  var i;
-  var x = document.getElementsByClassName("studies-img2");
-  if (n > x.length) {slideIndex2 = 1}
-  if (n < 1) {slideIndex2 = x.length}
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
-  }
-  x[slideIndex2-1].style.display = "block";  
-}
-
 //Hamburger Menu Scroll Functions
 
 const aboutMe = document.getElementById("aboutme");
@@ -97,19 +57,3 @@ function scrollToContact() {
 function scrollToQualifications() {
   qualifications.scrollIntoView({behavior: "smooth"});
 }
-
-//Collapsible
-document.querySelectorAll(".accordion").forEach(button => {
-  button.addEventListener('click', () => {
-    const  accordionContent = button.nextElementSibling;
-
-    button.classList.toggle('accordion--active');
-
-    if (button.classList.contains('accordion--active')) {
-      accordionContent.style.maxHeight = accordionContent.scrollHeight + 'px';
-    }
-    else {
-      accordionContent.style.maxHeight = 0;
-    }
-  });
-});
